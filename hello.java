1067d2cb97bbe8ec498bd24aca2242e8b75bd9e4
byui_student1045 <sco22022@byui.edu>
2025-03-20 11:24:15 -0600

    Created a test file for java

---
+++ hello.java
@@ -0,0 +1,10 @@
+/**
+ * Hello Class Main - Start with this file and edit as we go.
+ */
+
+ public class hello {
+    public static void main(String[] args) {
+        // Print the line of code to the output.
+        System.out.println("Hello World! Am I actually working?");
+    }
+ }
