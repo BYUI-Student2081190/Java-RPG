f97bdb9db306304c167f65fc1154a961b40413a7
byui_student1045 <sco22022@byui.edu>
2025-03-24 19:17:24 -0600

    Fixed a bug between menus in the code.

--- Game.java
+++ Game.java
@@ -9,7 +9,9 @@ import java.util.Scanner; // Import the scanner class to read inputs from user.
 
 public class Game {
     // Global Variables.
+    private static Scanner scanner = new Scanner(System.in); // Added Scanner as a global variable.
     public static int gameSpeed = 50; // This changes how fast text flows in the game. This is currrently 50 miliseconds.
+    // public final Character character; // Default = null. This is the character made by the user for the game.
         
     // This must be at the top, this function clears the console for better UI.
     public static void clearConsole() {
@@ -52,11 +54,83 @@ public class Game {
         }
     }
 
+    // This function contains the Character Menu.
+    public static void characterMenu() {
+        // Create new menu.
+        boolean leaveMenu = false;
+        String userInput; // Empty user input varible.
+
+        while (leaveMenu != true) {
+            printLetterByLetter("Welcome to the Character Creator!");
+            printLetterByLetter("Here you can make, save, and even load characters you like for the game.");
+            printLetterByLetter("What would you like to do today?");
+            printLetterByLetter("1.) Create New Character.");
+            printLetterByLetter("2.) Save Created Character.");
+            printLetterByLetter("3.) Load In Previous Character.");
+            printLetterByLetter("4.) Go Back to Main Menu.");
+            printLetterByLetter("Please type the number of the option you would like to do: ");
+            userInput = scanner.nextLine();
+            
+            // Check to see if input is a number.
+            if (isInt(userInput)) {
+                // Convert the type.
+                int choice = Integer.parseInt(userInput);
+
+                // Select the option.
+                switch (choice) {
+                    case 1:
+                        // Clear the console.
+                        clearConsole();
+                        // Call the character creator function.
+                        printLetterByLetter("Lets vreate them!");
+                        // Break the case.
+                        break;
+                    case 2:
+                        // Clear the console.
+                        clearConsole();
+                        // Call the save character function.
+                        printLetterByLetter("Lets save them!"); // Add this later.
+                        // Break the case.
+                        break;
+                    case 3:
+                        // Clear the console.
+                        clearConsole();
+                        // Call the load character function.
+                        printLetterByLetter("Lets load them!"); // Add this later.
+                        // Break the case.
+                        break;
+                    case 4:
+                        // Clear the console.
+                        clearConsole();
+                        // Set leave menu to true to leave the function.
+                        leaveMenu = true;
+                        // Break the case.
+                        break;
+                    default:
+                        // Clear the console.
+                        clearConsole();
+                        // Let the user know the mistake.
+                        printLetterByLetter("Your number was not one of the options. Please select one of the options.");
+                        printLetterByLetter("Press Enter to Continue...");
+                        scanner.nextLine();
+                        // Clear the console again.
+                        clearConsole();
+                }
+            } else {
+                // Give them the first error input.
+                clearConsole();
+                printLetterByLetter("Your input was not a number. Please enter the number of the option.");
+                printLetterByLetter("Press Enter to Continue...");
+                scanner.nextLine();
+                clearConsole();
+            }
+        }
+        // Now return to the previous function.
+        return;
+    }
+
     // Main class. This runs at the start.
     public static void main(String[] args) {
-        // Create new scanner to handle user inputs.
-        Scanner scanner = new Scanner(System.in);
-
         // Create a Title Screen for the player.
         System.out.println("JAVA QUEST\n");
         System.out.println("Press Enter to Start... ");
@@ -72,6 +146,8 @@ public class Game {
             printLetterByLetter("Welcome to the land of Javopia.\nA world full of wonder and adventure.");
             printLetterByLetter("Tell me adventurer? What would you like to do first?");
             printLetterByLetter("1.) Create Character");
+            // Add Quest Option. (Cannot go on a quest until the character has been created.)
+            // Add Settings Option.
             printLetterByLetter("2.) Quit");
             printLetterByLetter("Please type the number of the option you would like: ");
             userResponse = scanner.nextLine();
@@ -87,7 +163,7 @@ public class Game {
                         // Clear the console for next action.
                         clearConsole();
                         // Call the character creator function.
-                        printLetterByLetter("Lets create a character!");
+                        characterMenu();
                         break;
                     case 2:
                         // Clear the console for the next action.
@@ -114,8 +190,5 @@ public class Game {
 
         // Create an end message.
         printLetterByLetter("Thanks for playing! See you around!");
-
-        // After Game finishes close the scanner in use.
-        scanner.close();
     }
 }
