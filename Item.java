69c2266035bb54005e7da143bcb32e852697c387
byui_student1045 <sco22022@byui.edu>
2025-03-26 02:22:58 -0600

    Added the item class, this will come in later.

---
+++ Item.java
@@ -0,0 +1,9 @@
+/**
+ * This is the Item Class - This class is used to create items for the player to use on their journey.
+ * This class will hold item data, like if it is equipable and if it can be used to heal the player and such. They
+ * will always take up space in the player's inventory.
+ */
+
+public class Item {
+    
+}
